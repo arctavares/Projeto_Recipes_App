079fafad88fcde45cff3ba9b5a3b97a98f2edb07
arctavares <cosilk@hotmail.com>
2023-10-23 16:30:30 -0300

    fix setData delay error

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import styles from './index.module.css';
 
@@ -58,8 +58,11 @@ function SearchBar() {
       }
     }
     console.log(mealOrDrinksData);
-    setData(mealOrDrinksData);
   }
+
+  useEffect(() => {
+    setData(mealOrDrinksData);
+  }, [mealOrDrinksData]);
 
   function renderRadioContainer() {
     return (
