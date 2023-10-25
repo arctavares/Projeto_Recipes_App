bf30eedd5c38dc108ffc22e7a6fb8334baa6ecc9
arctavares <cosilk@hotmail.com>
2023-10-24 23:06:03 -0300

    req 18

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './index.module.css';
 import DrinkIcon from '../../images/icone-drink.png';
 import PlateIcon from '../../images/icone-prato.png';
@@ -5,8 +6,12 @@ import PlateIcon from '../../images/icone-prato.png';
 function Footer() {
   return (
     <div className={ styles.footerContainer } data-testid="footer">
-      <img src={ `${DrinkIcon}drinkIcon` } alt="Drink" data-testid="drinks-bottom-btn" />
-      <img src={ `${PlateIcon}mealIcon` } alt="Meal" data-testid="meals-bottom-btn" />
+      <Link to="/drinks">
+        <img src={ DrinkIcon } alt="Drink" data-testid="drinks-bottom-btn" />
+      </Link>
+      <Link to="/meals">
+        <img src={ PlateIcon } alt="Meal" data-testid="meals-bottom-btn" />
+      </Link>
     </div>
   );
 }
