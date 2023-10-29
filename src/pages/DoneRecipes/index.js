8358f45d19113ba1fa5b39bbc51708ebe06f2218
arctavares <cosilk@hotmail.com>
2023-10-29 15:29:45 -0300

    working on doneRecipes

--- src/pages/DoneRecipes/index.js
+++ src/pages/DoneRecipes/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import RecipesContext from '../../context';
 import AllDoneRecipe from '../../images/AllDoneRecipes.png';
@@ -10,8 +10,37 @@ function DoneRecipes() {
   const {
     setCurrentTitle,
   } = useContext(RecipesContext);
+  const [doneRecipes, setDoneRecipes] = useState([]);
 
   setCurrentTitle('Done Recipes');
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('doneRecipes'));
+    setDoneRecipes(data.doneRecipes);
+  }, []);
+
+  function returnCard(recipe) {
+    console.log(recipe);
+    return (
+      <div className={ styles.cardContainer }>
+        <div className={ styles.imgContainer }>
+          <img src={ recipe.strMealThumb } alt="recipe" />
+        </div>
+        <div>
+          <div className={ styles.titleAndSubtitle }>
+            <h1>{recipe.strMeal}</h1>
+            <h2>
+              {recipe.strArea}
+              {' '}
+              *
+              {' '}
+              {recipe.strCategory}
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -20,6 +49,15 @@ function DoneRecipes() {
         <img src={ AllDoneRecipe } alt="AllDoneRecipes" />
         <img src={ drinks } alt="Drinks" />
         <img src={ foods } alt="Foods" />
+      </div>
+      <div className={ styles.MainCardsContainer }>
+        {doneRecipes !== undefined
+        && doneRecipes.length !== 0
+        && doneRecipes.map((recipe) => (
+          <div className={ styles.cardsContainer } key={ recipe.idMeal }>
+            {returnCard(recipe)}
+          </div>
+        ))}
       </div>
     </>
   );
